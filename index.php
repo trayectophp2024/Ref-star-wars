e7aab068b8c3ad3af3bd24c6ff09cf98b4fb3a35
trayectophp2024 <trayectophp2024@gmail.com>
2024-09-24 17:00:56 -0300

    add header,footer,index

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php require "partials/header.php" ?>
+
+<main class="container">
+    <img width="100%" height="300px" src="img/banner.webp" alt="iocus banner">
+
+    <div class="row">
+        <h1 class="text-center mt-3">DESTACADOS</h1>
+
+        <div class="col-4 mt-4 mb-4">
+            <div class="card" style="width: 18rem;">
+                <img src="img/superman.webp" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Superman</h5>
+                    <h5 class="card-title text-success">$20.000</h5>
+                    <a href="" class="btn btn-primary">Ver</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="text-center">
+        <h2 class="text-center mt-3">CATEGORIAS</h2>
+
+        <div class="row">
+            <div class="col align-self-start">
+                <a href="" class="d-block mb-4">
+                    <img class="rounded-circle" width="200px" src="img/juegos-de-mesa-logo.webp" alt="">
+                </a>
+                <a href="" class="d-block mb-4">
+                    <img class="rounded-circle" width="200px" src="img/figuras-logo.webp" alt="">
+                </a>
+
+            </div>
+            <div class="col align-self-center">
+                <div>
+                    <img class="rounded-circle" width="400px" src="img/logo.webp" alt="">
+                </div>
+            </div>
+            <div class="col align-self-end">
+                <a href="" class="d-block mb-4">
+                    <img class="rounded-circle" width="200px" src="img/monopatin-logo.jpg" alt="">
+                </a>
+                <a href="" class="d-block mb-4">
+                    <img class="rounded-circle" width="200px" src="img/figuras-logo.webp" alt="">
+                </a>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+</main>
+
+
+
+
+<?php require "partials/footer.php" ?>
---
+++ partials/header.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>IOCUS - Inicio</title>
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+    <!-- Bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/estilos.css">
+
+    <!-- ICONOS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+</head>
+
+<body>
+
+    <!-- HEADER -->
+
+    <header class="container">
+        <nav class="navbar navbar-expand-lg bg-primary p-3" data-bs-theme="dark" >
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">IOCUS</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="index.php">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Nosotros</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Categorias
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Figuras</a></li>
+                                <li><a class="dropdown-item" href="#">Juegos de Mesa</a></li>
+                                <li><a class="dropdown-item" href="#">Monopatines</a></li>
+                            </ul>
+                        </li>
+                        
+                    </ul>
+                    <form action="busqueda.php" method="POST" class="d-flex" role="search">
+                        <div class="input-group">
+                            <span class="input-group-text">
+                                <i class="fa-solid fa-magnifying-glass"></i>      
+                            </span>
+                            <input class="form-control" name="q" type="search" placeholder="Search" aria-label="Search">
+                            <button class="boton-color" type="submit">Buscar</button>
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
