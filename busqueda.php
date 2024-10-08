e2cfd3dab1846f38b1120e57c7a650646beda99d
trayectophp2024 <trayectophp2024@gmail.com>
2024-10-08 14:19:09 -0300

    add search partial

--- busqueda.php
+++ busqueda.php
@@ -7,27 +7,37 @@ $termino_busqueda = $_GET['q'] ?? '';
 
 $productos = [];
 
-if($termino_busqueda){
+if ($termino_busqueda) {
     // si hay un termino busqueda, llamamos a la funciona buscar_producto
     $productos = buscar_productos($conn, $termino_busqueda);
 }
 
 ?>
 
+<?php require "partials/header.php" ?>
+
 <main class="container">
-     <h1 class="text-center">Resultados de la búsqueda</h1>
+    <h1 class="text-center">Resultados de la búsqueda</h1>
 
-    <?php if($termino_busqueda && !empty($productos)) { ?>
+    <?php if ($termino_busqueda && !empty($productos)) { ?>
         <div class="row">
-            <?php foreach($productos as $producto) { ?>
-                    <?= $producto['nombre'] ?>
+            <?php foreach ($productos as $producto) { ?>
+                <div class="col-4 mt-4 mb-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/<?= $producto['imagen'] ?>" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $producto['nombre'] ?></h5>
+                            <h5 class="card-title text-success">$<?= $producto['precio'] ?></h5>
+                            <a href="" class="btn btn-primary">Ver</a>
+                        </div>
+                    </div>
+                </div>
             <?php } ?>
-         </div>
-    
+        </div>
+
     <?php } ?>
-
-
-
 
 </main>
 
+
+<?php require "partials/footer.php" ?>
